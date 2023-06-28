52e912781848b5b55b3fabbfb8d4f38d76c9ef66
karimabdrakhmanov <j3t1x0h@gmail.com>
2023-06-28 18:58:52 +0600

    14

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -2,13 +2,7 @@ SELECT * FROM payment WHERE amount >=500;
 SELECT * FROM STUDENT WHERE YEAR(CURDATE())- YEAR(BIRTHDAY)>20;
 SELECT * FROM student where groupnumber = 10 and YEAR(CURDATE()) - YEAR(BIRTHDAY)<20;
 SELECT * FROM student WHERE Name like 'Mike%' OR groupnumber in (4,5,6);
-SELECT * FROM payment WHERE TIMESTAMPDIFF(MONTH, payment_date, CURDATE()) < 8;
+select * from payment where payment_date >= (SELECT DATEADD(month, -8, '2022-08-25') AS DateAdd);
 SELECT * FROM student WHERE Name like 'A%';
 SELECT * FROM student WHERE (Name like 'Roxi%' and groupnumber =4) OR  (Name like 'Tallie%' and groupnumber =9);
--- Select all Payments with amount more or equal 500
--- Select students older than 20
--- Select students from the same group (10) but younger than 20
--- Select Students with Name ‘Mike’ or from the group 4, 5, 6
--- Select Payments dated conducted in past 8 months
--- Select all students whose name starts with ‘A’
--- Select students with name = Roxi and group= 4 or name Tallie and group = 9
+
--- lesson7/src/main/resources/SOLUTION.sql
+++ lesson7/src/main/resources/SOLUTION.sql
@@ -1,5 +1,4 @@
 SELECT MARK FROM MARK WHERE MARK >6 ORDER BY MARK DESC;
-SELECT * FROM student where ID in (SELECT STUDENT_ID FROM MARK WHERE SUBJECT_ID =(SELECT ID FROM SUBJECT WHERE NAME LIKE 'Math'));
 SELECT * FROM PAYMENT WHERE AMOUNT < 300 ORDER BY AMOUNT ASC;
 SELECT * FROM PAYMENTTYPE ORDER BY NAME ASC;
 SELECT * FROM STUDENT ORDER BY NAME DESC;
@@ -8,6 +7,4 @@ SELECT * FROM STUDENT WHERE ID IN (SELECT STUDENT_ID FROM PAYMENT WHERE AMOUNT>1
 -- Select payments which is less than 300 and filter in ascending order;
 -- Select payment types filtered alphabetically;
 -- Select students filtered in reverse alphabetical order;
--- Select students who have at least
--- one payment more than 1000
--- and filter them by birthday date ASC.
+-- Select students who have at least one payment more than 1000 and filter them by birthday date ASC.
