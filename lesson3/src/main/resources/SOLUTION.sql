15c6f7de3f8d6b05664265ecd20484536bea3bc1
karimabdrakhmanov <j3t1x0h@gmail.com>
2023-06-28 14:52:53 +0600

    5

--- lesson3/src/main/resources/SOLUTION.sql
+++ lesson3/src/main/resources/SOLUTION.sql
@@ -1,20 +1,20 @@
-ALTER TABLE Student
-ALTER COLUMN birthday DATE NOT NULL;
+ALTER TABLE student
+ADD CONSTRAINT birthday_not CHECK (birthday is not null);
 
-ALTER TABLE Mark
-ALTER COLUMN mark INT NOT NULL CHECK (mark >= 1 AND mark <= 10),
-ALTER COLUMN student_id BIGINT NOT NULL,
-ALTER COLUMN subject_id BIGINT NOT NULL;
+ALTER TABLE mark
+    ADD CONSTRAINT markCheck CHECK (mark >= 1 AND mark <= 10),
+    ADD CONSTRAINT studnotnull CHECK (student_id is not null),
+    ADD CONSTRAINT studnotnull CHECK (subject_id is not null);
 
-ALTER TABLE Subject
-ALTER COLUMN grade INT NOT NULL CHECK (grade >= 1 AND grade <= 5);
+ALTER TABLE subject
+    ADD CONSTRAINT gradeCheck CHECK (grade >= 1 AND grade <= 5);
 
-ALTER TABLE PaymentType
+ALTER TABLE paymenttype
     ADD CONSTRAINT unique_name UNIQUE (name);
 
 
-ALTER TABLE Payment
-ALTER COLUMN type_id BIGINT NOT NULL,
-ALTER COLUMN amount DECIMAL NOT NULL,
-ALTER COLUMN date DATETIME NOT NULL;
+ALTER TABLE payment
+    ADD CONSTRAINT type_id_not CHECK (type_id is not null),
+    ADD CONSTRAINT amount_not CHECK (amount is not null),
+    ADD CONSTRAINT payment_date_not CHECK (payment_date is not null);
 
---
+++ lesson8/src/main/resources/SOLUTION.sql
@@ -0,0 +1,8 @@
+-- Select the youngest student's birthday;
+-- Find the earliest made payment's date;
+-- Find average mark by Math;
+-- Find min amount of payment for payment type “WEEKLY”.
+SELECT MAX(BIRTHDAY) FROM STUDENT;
+SELECT MIN(PAYMENT_DATE) FROM PAYMENT;
+SELECT AVG(MARK) FROM MARK WHERE SUBJECT_ID = (SELECT ID FROM SUBJECT WHERE NAME LIKE 'Math');
+SELECT MIN(AMOUNT) FROM PAYMENT WHERE TYPE_ID = (SELECT ID FROM PAYMENTTYPE WHERE NAME LIKE 'WEEKLY');
