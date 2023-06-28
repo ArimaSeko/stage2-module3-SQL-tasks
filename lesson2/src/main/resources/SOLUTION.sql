33eb759d22aad78f1d2b7ea0d81580a255695fd1
karimabdrakhmanov <j3t1x0h@gmail.com>
2023-06-28 18:01:35 +0600

    9

--- lesson2/src/main/resources/SOLUTION.sql
+++ lesson2/src/main/resources/SOLUTION.sql
@@ -1,38 +1,35 @@
-INSERT INTO student (name, birthday, groupnumber) VALUES
-                                                      ("John", '2005-01-01', 1),
-                                                      ("Chris", '2005-02-02', 1),
-                                                      ("Carl", '2005-03-03', 1),
-                                                      ("Oliver", '2006-04-04', 2),
-                                                      ("James", '2006-05-05', 2),
-                                                      ("Lucas", '2006-06-06', 2),
-                                                      ("Henry", '2006-07-07', 2),
-                                                      ("Jacob", '2007-08-08', 3),
-                                                      ("Logan", '2007-09-09', 3);
+-- Insert Students
+INSERT INTO student (name, groupnumber)
+VALUES ('John', 1), ('Chris', 1), ('Carl', 1),
+       ('Oliver', 2), ('James', 2), ('Lucas', 2), ('Henry', 2),
+       ('Jacob', 3), ('Logan', 3);
 
-INSERT INTO subject (name, description, grade) VALUES
-                                                   ("Art", 'A class where students learn about different art forms', 1),
-                                                   ("Music", 'A class where students learn about different musical instruments and how to play them', 1),
-                                                   ("Geography", 'A class where students learn about different countries and cultures', 2),
-                                                   ("History", 'A class where students learn about important events in history', 2),
-                                                   ("PE", 'A class where students learn about different physical activities', 3),
-                                                   ("Math", 'A class where students learn about different mathematical concepts', 3),
-                                                   ("Science", 'A class where students learn about different scientific concepts', 4),
-                                                   ("IT", 'A class where students learn about computers and technology', 4);
+-- Insert Subjects
+INSERT INTO subject (name, description, grade)
+VALUES ('Art and music', 'Art and music for 1 grade', 1),
+       ('Geography and history', 'Geography and history for 2 grade', 2),
+       ('PE and math', 'PE and math for 3 grade', 3),
+       ('Science and IT', 'Science and IT for 4 grade', 4),
+       ('Subject 1 for grade 5', 'Description for subject 1 grade 5', 5),
+       ('Subject 2 for grade 5', 'Description for subject 2 grade 5', 5);
 
-INSERT INTO paymenttype (name) VALUES
-                                   ('DAILY'),
-                                   ('WEEKLY'),
-                                   ('MONTHLY');
+-- Insert Payment Types
+INSERT INTO paymenttype (name) VALUES ('DAILY'), ('WEEKLY'), ('MONTHLY');
 
-INSERT INTO payment (type_id, amount, payment_date, student_id) VALUES
-                                                                    (2, 50, '2017-11-02 22:02:31', 1),
-                                                                    (3, 100, '2016-10-05 12:55:34', 2),
-                                                                    (2, 25, '2017-11-02 22:02:31', 3),
-                                                                    (1, 10, '2016-10-05 12:55:34', 4);
+-- Insert Payments
+INSERT INTO payment (type_id, amount, payment_date, student_id) SELECT pt.id, 100.00, NOW(), s.id FROM student s JOIN paymenttype pt ON pt.name = 'WEEKLY' WHERE s.name = 'John';
+INSERT INTO payment (type_id, amount, payment_date, student_id) SELECT pt.id, 500.00, NOW(), s.id FROM student s JOIN paymenttype pt ON pt.name = 'MONTHLY' WHERE s.name = 'Oliver';
+INSERT INTO payment (type_id, amount, payment_date, student_id) SELECT pt.id, 100.00, NOW(), s.id FROM student s JOIN paymenttype pt ON pt.name = 'WEEKLY' WHERE s.name = 'Henry';
 
-INSERT INTO mark (student_id, subject_id, mark) VALUES
-                                                    (2, 1, 8),
-                                                    (3, 2, 5),
-                                                    (4, 3, 9),
-                                                    (5, 4, 4),
-                                                    (6, 5, 9);
+INSERT INTO payment (type_id, amount, payment_date, student_id) SELECT pt.id, 10.00, NOW(), s.id FROM student s JOIN paymenttype pt ON pt.name = 'DAILY' WHERE s.name = 'James';
+
+-- Insert Marks
+INSERT INTO mark (student_id, subject_id, mark) SELECT s.id, sub.id, 8 FROM student s JOIN subject sub ON sub.name = 'Art and music' WHERE s.name = 'Chris';
+
+INSERT INTO mark (student_id, subject_id, mark) SELECT s.id, sub.id, 5 FROM student s JOIN subject sub ON sub.name = 'Geography and history' WHERE s.name = 'Oliver';
+
+INSERT INTO mark (student_id, subject_id, mark) SELECT s.id, sub.id, 9 FROM student s JOIN subject sub ON sub.name = 'Geography and history' WHERE s.name = 'James';
+
+INSERT INTO mark (student_id, subject_id, mark) SELECT s.id, sub.id, 4 FROM student s JOIN subject sub ON sub.name = 'PE and math' WHERE s.name = 'Jacob';
+
+INSERT INTO mark (student_id, subject_id, mark) SELECT s.id, sub.id, 9 FROM student s JOIN subject sub ON sub.name = 'PE and math' WHERE s.name = 'Logan';
--- lesson9/src/main/resources/SOLUTION.sql
+++ lesson9/src/main/resources/SOLUTION.sql
@@ -1,8 +1,3 @@
--- Select all Students from Table Students having average mark more than 8
 SELECT student.id, student.name FROM student INNER JOIN mark ON student.id = mark.student_id GROUP BY student.id HAVING AVG(mark.mark) > 8;
-
--- Select students (id and name) who have minimum mark more than 7
 SELECT student.id, student.name FROM student INNER JOIN mark ON student.id = mark.student_id GROUP BY student.id HAVING MIN(mark.mark) > 7;
-
--- Select all students(id and name) who paid more than 2 times during 2019
 SELECT student.id, student.name FROM student INNER JOIN payment ON student.id = payment.student_id WHERE YEAR(payment.payment_date) = 2019 GROUP BY student.id HAVING COUNT(payment.id) > 2;
